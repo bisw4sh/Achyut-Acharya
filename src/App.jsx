5f286f3453a82a1e80bbf5ce2069e9996db60894
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-11-07 16:03:07 +0545

    config files

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <button className="btn btn-accent btn-outline">Three</button>
+    </h1>
+  )
+}
